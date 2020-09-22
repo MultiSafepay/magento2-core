05bab884872cd4a4c9aa13d2c5a8b1a02e1eeccc
Vinod Sowdagar <53818758+Vinod-MultiSafepay@users.noreply.github.com>
2020-09-22 13:19:10 +0200

    Add refund id to the refund transaction request
    * Add refund id to the refund transaction request
    
    * Update Changelog.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Added
+- Added MultiSafepay refund_id to the refund transaction in the Magento Payment.
+
 ### Fixed
 - Added dependencies in module.xml and composer.json
 
---
+++ Gateway/Response/RefundResponseHandler.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is provided with Magento in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * Copyright © 2020 MultiSafepay, Inc. All rights reserved.
+ * See DISCLAIMER.md for disclaimer details.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace MultiSafepay\ConnectCore\Gateway\Response;
+
+use Magento\Payment\Gateway\Helper\SubjectReader;
+use Magento\Payment\Gateway\Response\HandlerInterface;
+use Magento\Sales\Api\Data\OrderPaymentInterface;
+
+class RefundResponseHandler implements HandlerInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function handle(array $handlingSubject, array $response)
+    {
+        $paymentDataObject = SubjectReader::readPayment($handlingSubject);
+
+        /** @var OrderPaymentInterface $payment */
+        $payment = $paymentDataObject->getPayment();
+
+        $payment->setTransactionId($response['refund_id']);
+        $payment->setIsTransactionClosed(true);
+
+        return $this;
+    }
+}
