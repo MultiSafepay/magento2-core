8cb017d059ea19a10ea91cbc1dc58dbf3526a2bd
Vinod Sowdagar <53818758+Vinod-MultiSafepay@users.noreply.github.com>
2021-01-25 17:28:54 +0100

    Fix order emails not being sent for backend orders (#293)

--- Gateway/Request/Builder/RedirectTransactionBuilder.php
+++ Gateway/Request/Builder/RedirectTransactionBuilder.php
@@ -17,6 +17,9 @@ declare(strict_types=1);
 
 namespace MultiSafepay\ConnectCore\Gateway\Request\Builder;
 
+use Magento\Framework\App\Area;
+use Magento\Framework\App\State;
+use Magento\Framework\Exception\LocalizedException;
 use Magento\Payment\Gateway\Helper\SubjectReader;
 use Magento\Payment\Gateway\Request\BuilderInterface;
 use Magento\Sales\Model\Order;
@@ -37,20 +40,30 @@ class RedirectTransactionBuilder implements BuilderInterface
     private $config;
 
     /**
+     * @var State
+     */
+    private $state;
+
+    /**
      * RedirectTransactionBuilder constructor.
      *
+     * @param Config $config
+     * @param State $state
      * @param StatusResolver $statusResolver
      */
     public function __construct(
         Config $config,
+        State $state,
         StatusResolver $statusResolver
     ) {
         $this->config = $config;
         $this->statusResolver = $statusResolver;
+        $this->state = $state;
     }
 
     /**
      * @inheritDoc
+     * @throws LocalizedException
      */
     public function build(array $buildSubject): array
     {
@@ -65,6 +78,12 @@ class RedirectTransactionBuilder implements BuilderInterface
         $stateObject->setState($state);
         $stateObject->setStatus($orderStatus);
 
+        // Early return on backend order
+        if ($this->state->getAreaCode() === Area::AREA_ADMINHTML) {
+            return [];
+        }
+
+        // If not backend order, check when order confirmation e-mail needs to be sent
         if ($this->config->getOrderConfirmationEmail() !== 'before_transaction') {
             $stateObject->setIsNotified(false);
 
