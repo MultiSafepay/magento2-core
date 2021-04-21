f9d3764af6b6644dceac35afc492a4057227259f
Stanislau Kharlap <stanislau.kharlap@multisafepay.com>
2021-04-21 19:46:47 +0200

    added notifications about new version

--- Util/VersionUtil.php
+++ Util/VersionUtil.php
@@ -19,6 +19,23 @@ namespace MultiSafepay\ConnectCore\Util;
 
 class VersionUtil
 {
+    private const MULTISAFEPAY_MAGENTO_GITHUB_REPO_LINK
+        = 'https://api.github.com/repos/multisafepay/magento2/releases/latest';
+
+    /**
+     * @var JsonHandler
+     */
+    private $jsonHandler;
+
+    /**
+     * VersionUtil constructor.
+     *
+     * @param JsonHandler $jsonHandler
+     */
+    public function __construct(JsonHandler $jsonHandler)
+    {
+        $this->jsonHandler = $jsonHandler;
+    }
 
     /**
      * @return string
@@ -26,5 +43,43 @@ class VersionUtil
     public function getPluginVersion(): string
     {
         return '2.7.1';
+    }
+
+    /**
+     * @return array
+     */
+    public function getNewVersionsDataIfExist(): array
+    {
+        $options = [
+            'http' => [
+                'method' => "GET",
+                'header' => "Accept-language: en\r\n" .
+                            "Cookie: foo=bar\r\n" .
+                            "User-Agent: PHP\r\n",
+            ],
+        ];
+
+        $content = file_get_contents(
+            self::MULTISAFEPAY_MAGENTO_GITHUB_REPO_LINK,
+            false,
+            stream_context_create($options)
+        );
+
+        if ($content) {
+            $pluginData = $this->jsonHandler->readJSON($content);
+            $latestVersionRelease = $pluginData['tag_name'] ?? null;
+
+            if ($latestVersionRelease
+                && version_compare($latestVersionRelease, $this->getPluginVersion(), '>')
+            ) {
+                return [
+                    'version' => (string)$latestVersionRelease,
+                    'changelog' => $pluginData['body'] ?? '',
+                    'url' => $pluginData['html_url'] ?? '',
+                ];
+            }
+        }
+
+        return [];
     }
 }
