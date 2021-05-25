f6d5c694d6c4782c45f932d6a03f1098af406762
Stanislau Kharlap <stanislau.kharlap@multisafepay.com>
2021-05-25 16:08:04 +0200

    code refactoring + fixed issue with combined filters and getting wrong customer group id

--- Observer/ShipmentSaveAfterObserver.php
+++ Observer/ShipmentSaveAfterObserver.php
@@ -25,7 +25,7 @@ use Magento\Sales\Api\Data\ShipmentInterface;
 use MultiSafepay\Api\Transactions\UpdateRequest;
 use MultiSafepay\ConnectCore\Factory\SdkFactory;
 use MultiSafepay\ConnectCore\Logger\Logger;
-use MultiSafepay\ConnectCore\Util\PaymentMethodUtil;
+use MultiSafepay\ConnectCore\Util\PaymentMethodUtilTest;
 use MultiSafepay\Exception\ApiException;
 use Psr\Http\Client\ClientExceptionInterface;
 
@@ -53,7 +53,7 @@ class ShipmentSaveAfterObserver implements ObserverInterface
     private $messageManager;
 
     /**
-     * @var PaymentMethodUtil
+     * @var PaymentMethodUtilTest
      */
     private $paymentMethodUtil;
 
@@ -64,14 +64,14 @@ class ShipmentSaveAfterObserver implements ObserverInterface
      * @param Logger $logger
      * @param ManagerInterface $messageManager
      * @param UpdateRequest $updateRequest
-     * @param PaymentMethodUtil $paymentMethodUtil
+     * @param PaymentMethodUtilTest $paymentMethodUtil
      */
     public function __construct(
         SdkFactory $sdkFactory,
         Logger $logger,
         ManagerInterface $messageManager,
         UpdateRequest $updateRequest,
-        PaymentMethodUtil $paymentMethodUtil
+        PaymentMethodUtilTest $paymentMethodUtil
     ) {
         $this->sdkFactory = $sdkFactory;
         $this->logger = $logger;
--- Service/OrderService.php
+++ Service/OrderService.php
@@ -44,7 +44,7 @@ use MultiSafepay\ConnectCore\Model\Vault;
 use MultiSafepay\ConnectCore\Util\GiftcardUtil;
 use MultiSafepay\ConnectCore\Util\JsonHandler;
 use MultiSafepay\ConnectCore\Util\OrderStatusUtil;
-use MultiSafepay\ConnectCore\Util\PaymentMethodUtil;
+use MultiSafepay\ConnectCore\Util\PaymentMethodUtilTest;
 use MultiSafepay\Exception\ApiException;
 use Psr\Http\Client\ClientExceptionInterface;
 
@@ -66,7 +66,7 @@ class OrderService
     private $vault;
 
     /**
-     * @var PaymentMethodUtil
+     * @var PaymentMethodUtilTest
      */
     private $paymentMethodUtil;
 
@@ -136,7 +136,7 @@ class OrderService
      * @param OrderRepositoryInterface $orderRepository
      * @param EmailSender $emailSender
      * @param Vault $vault
-     * @param PaymentMethodUtil $paymentMethodUtil
+     * @param PaymentMethodUtilTest $paymentMethodUtil
      * @param SecondChance $secondChance
      * @param Logger $logger
      * @param OrderPaymentRepositoryInterface $orderPaymentRepository
@@ -154,7 +154,7 @@ class OrderService
         OrderRepositoryInterface $orderRepository,
         EmailSender $emailSender,
         Vault $vault,
-        PaymentMethodUtil $paymentMethodUtil,
+        PaymentMethodUtilTest $paymentMethodUtil,
         SecondChance $secondChance,
         Logger $logger,
         OrderPaymentRepositoryInterface $orderPaymentRepository,
