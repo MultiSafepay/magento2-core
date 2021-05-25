dc4470f6e69961c09a0e6f75736d0cae37e83e1b
Stanislau Kharlap <stanislau.kharlap@multisafepay.com>
2021-05-25 16:10:22 +0200

    revert back unnecessary changes

--- Service/OrderService.php
+++ Service/OrderService.php
@@ -44,7 +44,7 @@ use MultiSafepay\ConnectCore\Model\Vault;
 use MultiSafepay\ConnectCore\Util\GiftcardUtil;
 use MultiSafepay\ConnectCore\Util\JsonHandler;
 use MultiSafepay\ConnectCore\Util\OrderStatusUtil;
-use MultiSafepay\ConnectCore\Util\PaymentMethodUtilTest;
+use MultiSafepay\ConnectCore\Util\PaymentMethodUtil;
 use MultiSafepay\Exception\ApiException;
 use Psr\Http\Client\ClientExceptionInterface;
 
@@ -66,7 +66,7 @@ class OrderService
     private $vault;
 
     /**
-     * @var PaymentMethodUtilTest
+     * @var PaymentMethodUtil
      */
     private $paymentMethodUtil;
 
@@ -136,7 +136,7 @@ class OrderService
      * @param OrderRepositoryInterface $orderRepository
      * @param EmailSender $emailSender
      * @param Vault $vault
-     * @param PaymentMethodUtilTest $paymentMethodUtil
+     * @param PaymentMethodUtil $paymentMethodUtil
      * @param SecondChance $secondChance
      * @param Logger $logger
      * @param OrderPaymentRepositoryInterface $orderPaymentRepository
@@ -154,7 +154,7 @@ class OrderService
         OrderRepositoryInterface $orderRepository,
         EmailSender $emailSender,
         Vault $vault,
-        PaymentMethodUtilTest $paymentMethodUtil,
+        PaymentMethodUtil $paymentMethodUtil,
         SecondChance $secondChance,
         Logger $logger,
         OrderPaymentRepositoryInterface $orderPaymentRepository,
