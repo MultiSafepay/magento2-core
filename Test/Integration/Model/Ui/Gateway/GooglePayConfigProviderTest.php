4de1c6504304874c25dad31a380ff8a8ca4ebc7b
Stanislau Kharlap <stanislau.kharlap@multisafepay.com>
2021-10-14 16:10:53 +0200

    added couple of tests for GooglePayConfigProvider

---
+++ Test/Integration/Model/Ui/Gateway/GooglePayConfigProviderTest.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is provided with Magento in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * Copyright © 2020 MultiSafepay, Inc. All rights reserved.
+ * See DISCLAIMER.md for disclaimer details.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace MultiSafepay\ConnectCore\Test\Integration\Model\Ui\Gateway;
+
+use MultiSafepay\ConnectCore\Test\Integration\AbstractTestCase;
+use MultiSafepay\ConnectCore\Model\Ui\Gateway\GooglePayConfigProvider;
+
+/**
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class GooglePayConfigProviderTest extends AbstractTestCase
+{
+    /**
+     * @var GooglePayConfigProvider
+     */
+    private $googlePayConfigProvider;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->googlePayConfigProvider = $this->getObjectManager()->get(GooglePayConfigProvider::class);
+    }
+
+    /**
+     * @magentoConfigFixture default_store payment/multisafepay_googlepay/direct_button_mode 1
+     */
+    public function testGetGooglePlayMode(): void
+    {
+        self::assertSame(
+            GooglePayConfigProvider::GOOGLE_PAY_PRODUCTION_MODE,
+            $this->googlePayConfigProvider->getGooglePayMode()
+        );
+    }
+
+    /**
+     * @magentoConfigFixture default_store payment/multisafepay_googlepay/direct_button_account_id 12345
+     */
+    public function testGetMultisafepayAccountId(): void
+    {
+        self::assertSame('12345', $this->googlePayConfigProvider->getMultisafepayAccountId());
+    }
+
+    public function testGetMultisafepayAccountIdEmpty(): void
+    {
+        self::assertSame('', $this->googlePayConfigProvider->getMultisafepayAccountId());
+    }
+
+    /**
+     * @magentoConfigFixture default_store payment/multisafepay_googlepay/direct_button_merchant_name test_name
+     * @magentoConfigFixture default_store payment/multisafepay_googlepay/direct_button_merchant_id test_merchant_id
+     */
+    public function testGetGooglePayMerchantInfo(): void
+    {
+        self::assertEquals(
+            [
+                'merchantName' => 'test_name',
+                'merchantId' => 'test_merchant_id',
+            ],
+            $this->googlePayConfigProvider->getGooglePayMerchantInfo()
+        );
+    }
+
+    public function testGetGooglePayMerchantInfoEmpty(): void
+    {
+        self::assertEquals(
+            [
+                'merchantName' => '',
+                'merchantId' => '',
+            ],
+            $this->googlePayConfigProvider->getGooglePayMerchantInfo()
+        );
+    }
+}
