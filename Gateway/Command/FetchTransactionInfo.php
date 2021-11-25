4d0acf618917312e3aa430b838de31230c63ee0a
Stanislau Kharlap <78361324+Stanislau-MultiSafepay@users.noreply.github.com>
2021-11-25 10:37:37 +0100

    added possibility to fetch transaction data (#480)
    * added possibility to fetch transaction data for Visa payment method
    
    * added possibility to fetch transaction data for other payment methods

---
+++ Gateway/Command/FetchTransactionInfo.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is provided with Magento in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * Copyright © 2021 MultiSafepay, Inc. All rights reserved.
+ * See DISCLAIMER.md for disclaimer details.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace MultiSafepay\ConnectCore\Gateway\Command;
+
+use Magento\Framework\DataObject;
+use Magento\Payment\Gateway\CommandInterface;
+use Magento\Payment\Model\InfoInterface;
+use MultiSafepay\ConnectCore\Factory\SdkFactory;
+use MultiSafepay\ConnectCore\Logger\Logger;
+use MultiSafepay\Exception\ApiException;
+use MultiSafepay\Exception\InvalidApiKeyException;
+use Psr\Http\Client\ClientExceptionInterface;
+
+class FetchTransactionInfo extends DataObject implements CommandInterface
+{
+    /**
+     * @var SdkFactory
+     */
+    private $sdkFactory;
+
+    /**
+     * @var Logger
+     */
+    private $logger;
+
+    /**
+     * FetchTransactionInfo constructor.
+     *
+     * @param SdkFactory $sdkFactory
+     * @param Logger $logger
+     * @param array $data
+     */
+    public function __construct(
+        SdkFactory $sdkFactory,
+        Logger $logger,
+        array $data = []
+    ) {
+        $this->sdkFactory = $sdkFactory;
+        $this->logger = $logger;
+        parent::__construct($data);
+    }
+
+    /**
+     * @param array $commandSubject
+     * @return array
+     */
+    public function execute(array $commandSubject): array
+    {
+        /** @var InfoInterface $payment */
+        $payment = $commandSubject['payment']->getPayment();
+        $order = $payment->getOrder();
+        $orderId = $order->getIncrementId();
+        $result = [];
+
+        try {
+            $transactionManager = $this->sdkFactory->create((int)$order->getStoreId())->getTransactionManager();
+            $transactionResponse = $transactionManager->get($orderId);
+            $transaction = $transactionResponse->getData();
+
+            unset(
+                $transaction['checkout_options'],
+                $transaction['costs'],
+                $transaction['custom_info'],
+                $transaction['customer'],
+                $transaction['payment_details'],
+                $transaction['payment_methods'],
+                $transaction['shopping_cart']
+            );
+
+            array_walk_recursive($transaction, static function ($value, $key) use (&$result) {
+                $result[$key] = $value;
+            });
+        } catch (ApiException $apiException) {
+            $this->logger->logExceptionForOrder($orderId, $apiException);
+        } catch (InvalidApiKeyException $invalidApiKeyException) {
+            $this->logger->logInvalidApiKeyException($invalidApiKeyException);
+        } catch (ClientExceptionInterface $clientException) {
+            $this->logger->logClientException($orderId, $clientException);
+        }
+
+        return $result;
+    }
+}
