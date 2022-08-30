c01fe05f7de528b0e7aafc6f3b056d6ddf10ce13
Daniel Civit <daniel.civit@multisafepay.com>
2022-08-30 14:28:48 +0200

    PLGMAG2V2-535: Fix integration tests (#542)

--- .github/workflows/magento-integration-tests-pre-install.sh
+++ .github/workflows/magento-integration-tests-pre-install.sh
@@ -6,9 +6,18 @@ if [[ $GITHUB_REPOSITORY == *"internal"* ]] ; then
     REPO_SUFFIX="-internal"
 fi
 
-composer config repositories.multisafepay-php-sdk vcs git@github.com:MultiSafepay/php-sdk${REPO_SUFFIX}.git
+if [[ $(curl -s -u ${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN https://api.github.com/repos/MultiSafepay/php-sdk${REPO_SUFFIX}/branches | grep -iGc '"name": "'${CURRENT_HEAD_REF}'"') == 1 ]]; then
+    BRANCH_NAME=${CURRENT_HEAD_REF}
+else
+    BRANCH_NAME="master"
+fi
+
+git clone -b ${BRANCH_NAME} --single-branch https://${GITHUB_ACTOR}:$GLOBAL_GITHUB_TOKEN@github.com/MultiSafepay/php-sdk${REPO_SUFFIX}.git ./package-source/multisafepay/php-sdk/
+
+composer config repositories.multisafepay "path" "package-source/multisafepay/*"
 
 composer config minimum-stability dev
 composer config prefer-stable false
+composer update
 
 composer require yireo/magento2-replace-bundled:^4.1 --no-update
