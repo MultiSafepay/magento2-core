04ac8814274b3c475ca98cdbee156e7569d9ed91
Vinod Sowdagar <53818758+Vinod-MultiSafepay@users.noreply.github.com>
2024-02-22 16:02:56 +0100

    PLGMAG2V2-731: Add VVV Cadeaubon (#677)

---
+++ Model/Ui/Giftcard/VVVcadeaubonConfigProvider.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is provided with Magento in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * See DISCLAIMER.md for disclaimer details.
+ */
+
+declare(strict_types=1);
+
+namespace MultiSafepay\ConnectCore\Model\Ui\Giftcard;
+
+use MultiSafepay\ConnectCore\Model\Ui\GenericGiftcardConfigProvider;
+
+class VVVcadeaubonConfigProvider extends GenericGiftcardConfigProvider
+{
+    public const CODE = 'multisafepay_vvvcadeaubon';
+}
